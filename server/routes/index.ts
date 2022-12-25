a0c8ddeb05ae98be27ea9ebc515e4597a5adaa24
Alex Cândido <alex.candido.tec@gmail.com>
2022-12-25 20:00:55 -0300

    estrutura das rotas em server

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,8 +2,24 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World")
+router.get("/tasks", (req, res) => {
+  res.send("getting tasks!")
+})
+
+router.post("/tasks", (req, res) => {
+  res.send("creating tasks!")
+})
+
+router.get("/tasks/:id", (req, res) => {
+  res.send("getting a task!")
+})
+
+router.delete("/tasks/:id", (req, res) => {
+  res.send("deleting a task!")
+})
+
+router.put("/tasks/:id", (req, res) => {
+  res.send("updating a task!")
 })
 
 export default router;
