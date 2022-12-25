ea13227d38c59db577ff0ec7253e1d7650b4b606
Alex Cândido <alex.candido.tec@gmail.com>
2022-12-25 19:23:30 -0300

    configurações iniciais server

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,14 @@
 import cors from 'cors';
 import express from "express";
 import morgan from 'morgan';
+import router from './routes';
 
 const app = express();
 
-app.use(express.json())
 app.use(cors());
 app.use(morgan("dev"));
+app.use(express.json())
 
-app.get("/home", (req, res) => {
-  return res.json("Hello World")
-})
+app.use("/api", router);
 
 export default app;
