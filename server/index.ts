bbb617dd2f82e8cdb3a76a6de052e7476a185856
Alex Cândido <alex.candido.tec@gmail.com>
2022-12-27 16:46:59 -0300

    adicionado .env e regras package.json em server

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
+import 'dotenv/config';
 import app from './app';
 import { startConnection } from './database';
 
 startConnection();
-app.listen(3000, () => console.log('Server is running on port 3000'))
+app.listen(process.env.PORT, () => console.log('Server is running on port 3000'))
 
