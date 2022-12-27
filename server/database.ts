679e2cb500e99bb71f092d06e197eb37a86fc55a
Alex Cândido <alex.candido.tec@gmail.com>
2022-12-27 20:10:38 -0300

    configurações de services/axios

--- server/database.ts
+++ server/database.ts
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 export const startConnection = async (): Promise<void> => {
   try {
-    const db = await connect("mongodb+srv://mevn:mevn123@cluster0.k1xui7n.mongodb.net/Task?retryWrites=true&w=majority");
+    const db = await connect(`mongodb+srv://mevn:${process.env.DB_PASS}@cluster0.k1xui7n.mongodb.net/Task?retryWrites=true&w=majority`);
     console.log(db.connection.name);
   } catch (error) {
     console.error(error);
