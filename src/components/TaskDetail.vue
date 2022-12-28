b44bb00de7797757d73335aec90a0a02f1d097ef
Alex Cândido <alex.candido.tec@gmail.com>
2022-12-28 00:24:59 -0300

    metodo handleUpdate em TaskDetail

--- src/components/TaskDetail.vue
+++ src/components/TaskDetail.vue
@@ -1,17 +1,22 @@
 <template>
   <h1>Task detail</h1>
 
-  <form action="">
+  <form @submit.prevent="handleUpdate()">
     <input type="text" v-model="currentTask.title">
 
     <textarea rows="3" v-model="currentTask.description"></textarea>
+
+    <button>Update</button>
   </form>
+
+
+  <button>Delete</button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { Task } from '../interfaces/Task';
-import { getTask } from '../services/TaskService';
+import { getTask, updateTask } from '../services/TaskService';
 
 export default defineComponent({
   data() {
@@ -23,6 +28,12 @@ export default defineComponent({
     async loadTask(id: string) {
       const res = await getTask(id);
       this.currentTask = res.data
+    },
+    async handleUpdate() {
+      if (typeof this.$route.params.id === "string") {
+        const res = await updateTask(this.$route.params.id, this.currentTask);
+        console.log(res)
+      }
     }
   },
   mounted() {
