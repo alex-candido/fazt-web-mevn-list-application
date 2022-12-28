6f7dc610e582f653ba24e8e3b795448a9316625c
Alex Cândido <alex.candido.tec@gmail.com>
2022-12-28 00:53:53 -0300

    estilos com Bootswatch em Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,18 @@
 <template>
-  <nav>
-    <ul>
-      <li>
-        <router-link to="/">tasks</router-link>
-      </li>
-      <li>
-        <router-link to="/tasks/new">create task</router-link>
-      </li>
-    </ul>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+      <router-link class="navbar-brand" to="/">Vue Typescript</router-link>
+      <div>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">tasks</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/tasks/new">create task</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
 </template>
 
